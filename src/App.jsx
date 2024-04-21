240b58f775921c41a936eeed2271f0e8d7ca7c8a
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-04-21 15:08:15 +0000

    Borda à esquerda do título, tags e respectivos cards, animação de pointer.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="card">
       <h2>Rick Sanchez</h2>
+      <p class="title1">Status: Vivo</p>
+      <p class="title2">Espécie: Humana</p>
+      <p class="title3">Origem: Terra C-137</p>
       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
     </div>
   )
