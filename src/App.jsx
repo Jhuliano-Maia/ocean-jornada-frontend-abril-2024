f84a2e19958611954e4c51b0b2aa8d13cd3eb7d1
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-04-25 22:13:04 +0000

    Criação do componente Card e adição de props para o componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
+import Card from "./components/Card/Card.jsx"
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
   return (
-    <div className="card">
-      <h2>Rick Sanchez</h2>
-      <p class="title1">Status: Vivo</p>
-      <p class="title2">Espécie: Humana</p>
-      <p class="title3">Origem: Terra C-137</p>
-      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-    </div>
+    <>
+      <Card item={item1}/>
+      <Card item={item2}/>
+      <Card item={item3}/>
+    </>
   )
 }
 
