83b362112587b3b9cd43cdb298ea4771352d0ea7
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-04-25 23:27:44 +0000

    Criação de componentes para tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import './App.css'
-import Card from "./components/Card/Card.jsx"
+import Card from './components/Card/Card'
 
 function App() {
   const item1 = {
     name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
   }
 
   const item2 = {
     name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    tags: ['Status: Vivo']
   }
 
   const item3 = {
     name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+    tags: ['Espécie: Humana']
   }
 
- const itens = [item1, item2, item3] 
+  const itens = [item1, item2, item3]
 
   return (
     <>
-      {itens.map(function (elemento) {
-        return <Card item={elemento} />
-      })}
+      <div className="cards">
+        {itens.map(function (elemento) {
+          return <Card item={elemento} />
+        })}
+      </div>
     </>
   )
 }
