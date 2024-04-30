e9b2d5b0e08d66bb10ae40b7a7e2e6ae173032f0
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-04-30 23:02:51 +0000

    Integração com API do Rick and Morty e correção de erros das keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  }
+  const [itens, setItens] = useState([])
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-    tags: ['Status: Vivo']
-  }
+  const apiUrl = 'https://rickandmortyapi.com/api/character/'
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-    tags: ['Espécie: Humana']
-  }
+  useEffect(function () {
+    async function carregarDados() {
+      const response = await fetch(apiUrl)
 
-  const itens = [item1, item2, item3]
+      const data = await response.json()
+
+      const results = data.results
+      setItens(results)
+    }
+
+    carregarDados()
+  }, [])
 
   return (
     <>
       <div className="cards">
         {itens.map(function (elemento) {
-          return <Card item={elemento} />
+          return <Card item={elemento} key={`card_$(elemento.id)`} />
         })}
       </div>
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,8 +8,8 @@ export default function Card(props) {
             <h2>{item.name}</h2>
 
             <div className="tags">
-                {item.tags.map(function (tag) {
-                    return <Tag text={tag} />
+                {item.tags?.map(function (tag) {
+                    return <Tag text={tag} key={`tag_$(tag)`}/>
                 })}
             </div>
 
