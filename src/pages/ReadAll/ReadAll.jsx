0c5fe1d705867dd4b486cf12ae0bc4f5b8f9c986
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-05-01 00:03:14 +0000

    Correção de import

--- src/pages/ReadAll/ReadAll.jsx
+++ src/pages/ReadAll/ReadAll.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react'
-import Card from './components/Card/Card'
+import Card from '../../components/Card/Card'
 
 export default function ReadAll() {
     const [itens, setItens] = useState([])
 
-  const apiUrl = 'https://rickandmortyapi.com/api/character/'
+    const apiUrl = 'https://rickandmortyapi.com/api/character/'
 
-  useEffect(function () {
-    async function carregarDados() {
-      const response = await fetch(apiUrl)
+    useEffect(function () {
+      async function carregarDados() {
+        const response = await fetch(apiUrl)
 
-      const data = await response.json()
+        const data = await response.json()
 
-      const results = data.results
-      setItens(results)
-    }
+        const results = data.results
+        setItens(results)
+      }
 
-    carregarDados()
-  }, [])
+      carregarDados()
+    }, [])
 
-  return (
-    <>
-      <div className="cards">
-        {itens.map(function (elemento) {
-          return <Card item={elemento} key={`card_$(elemento.id)`} />
-        })}
-      </div>
-    </>
-  )
+    return (
+      <>
+        <div className="cards">
+          {itens.map(function (elemento) {
+            return <Card item={elemento} key={`card_${elemento.id}`} />
+          })}
+        </div>
+      </>
+    )
 }
