6fef3066e06a979c90fa40a274bd92d164b0fdb0
Jhuliano-Maia <165965355+Jhuliano-Maia@users.noreply.github.com>
2024-05-02 22:38:38 +0000

    Implementação do onSubmit pegando os dados do form e transformando em JSON

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,3 +1,39 @@
-export default function () {
-    return <div>Create</div>
-}
+import { Link } from "react-router-dom";
+
+export default function Create() {
+    function onSubmit(event) {
+        event.preventDefault()
+    
+        const name = event.target.name.value
+        const image = event.target.image.value
+    
+        const data = {
+          name,
+          image
+        }
+    
+        const json = JSON.stringify(data)
+    
+        console.log(data, json)
+    
+      }
+
+    return (
+        <>
+          <Link to="/">Home</Link>
+          <div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" id="name" name="name" placeholder="Digite o nome" />
+                <br />
+    
+                <label htmlFor="image">Imagem:</label>
+                <input type="text" id="image" name="image" placeholder="Digite a URL da imagem" />
+                <br />/>
+    
+                <input type="submit" value="Criar" />
+            </form>
+          </div>
+        </>
+      )
+    }
